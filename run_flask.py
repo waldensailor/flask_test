f7ce4b1fdb9c2ce3eba374c4121c47f73e460680
waldensailor <1713933120@qq.com>
2017-11-11 20:59:58 +0800

    fdf

--- run_flask.py
+++ run_flask.py
@@ -2,10 +2,16 @@
 
 # -*- coding:utf-8 -*-
 
-
-from flask import Flask, url_for, render_template
+import os
+from flask import Flask, url_for, render_template, request, redirect
 from flask_jsonrpc import JSONRPC
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/home/churen/work'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 jsonrpc = JSONRPC(app, '/api')
 
 @app.route('/')
@@ -17,10 +23,35 @@ def hello():
 def index():
     return u'Welcome to Flask JSON-RPC'
 @app.route('/san/<username>')
-def txt(username):
+def txt():
     return username
+                
+def allowed_file(filename):
+    return '.' in filename and \
+               filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
+@app.route('/up', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print('hello')
+        file_ = request.files['file']
+        print('的到了 file')
+        if file_ and allowed_file(file_.filename):
+            filename = secure_filename(file_.filename)
+            print('存开x')
+            file_.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print('33232')
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+            <!doctype html>
+                                                                                                                        <title>Upload new File</title>
+                                                                                                                            <h1>Upload new File</h1>
+                                                                                                                                <form action="" method=post enctype=multipart/form-data>
+                                                                                                                                      <p><input type=file name=file>
+                                                                                                                                               <input type=submit value=Upload>
+                                                                                                                                                   </form>
+                                                                                                                                                       '''
 with app.test_request_context():
     print(url_for('txt', username='tyan'))
 
